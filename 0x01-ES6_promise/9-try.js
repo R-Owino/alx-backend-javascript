3f0c265d99f7aa2eab0de1b0464c3610487a1ffb
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 23:29:32 +0000

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,18 +1,16 @@
 /* creates and returns an array named queue */
 
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
 
   return queue;
 }
-
-export default guardrail;
