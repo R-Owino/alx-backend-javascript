6c5349bb3b3a5b620c43bb553b3bcf20e9a3fabc
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 08:10:42 +0000

    Solve var hoisting problem

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+/* modifies variables inside the function taskBlock so that
+ * the variables aren’t overwritten inside the conditional block
+ */
+
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
