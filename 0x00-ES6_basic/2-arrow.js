4510d2529db3fc15a717003ea624b86fde21c480
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 09:10:18 +0000

    Standard function to arrow function: Add

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* Rewrite std function to arrow function */
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
