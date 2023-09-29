eea25ae7d111490f9aa7be1b181bc16c1934ac68
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 19:46:17 +0000

    Report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+/* Function whose param employeeList is the return value of the previous function */
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
