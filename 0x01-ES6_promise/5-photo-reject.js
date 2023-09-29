f12b7d47acf2b65166e731e66240365841103429
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 22:29:02 +0000

    Reject a promise

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+/* return a Promise rejecting with an Error and the string $fileName cannot be processed */
+
+export default function uploadPhoto(filename) {
+  return new Promise((reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
+}
