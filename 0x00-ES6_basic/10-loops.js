a5ef80ab9029d7655f72b873bfd9287629c2b0ba
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 19:16:24 +0000

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* ES6's for...of loop */
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
