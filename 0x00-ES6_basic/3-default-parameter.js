dee14ab2bceae4825e4aebeba10badc1deda0101
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 09:32:12 +0000

    Using default parameters

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+/* Condense the internals without changing the name of each function/variable */
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
