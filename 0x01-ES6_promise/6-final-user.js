b892417451b426354f3f1d80e90152065c6091a4
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-09-29 22:51:10 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,29 +3,17 @@
     * firstName (string)
     * lastName (string)
     * fileName (string)
- * returns an array with this structure:
- * [
-    {
-      status: status_of_the_promise,
-      value: value or error returned by the Promise
-    },
-    ...
-  ]
  */
 
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const output = [];
-      results.forEach((result) => {
-        output.push({
-          status: result.status,
-          value: result.value,
-        });
-      });
-      return output;
-    });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)])
+    .then((value) => value.map((result) => ({
+      status: result.status,
+      value: result.value,
+    })));
 }
