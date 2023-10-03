26eabb27238589ba7c0a62ee5b952fe7a6f380f9
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-10-03 22:58:24 +0000

    List of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,11 +2,13 @@
  * Each object has 3 attributes: id, firstName, location
  */
 
-export default function getListStudents() {
+const getListStudents = () => {
   const arr = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return arr;
-}
+};
+
+export default getListStudents;
