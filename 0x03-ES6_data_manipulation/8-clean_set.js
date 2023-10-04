c8172a32d5485301e4a80e4f49b842ac9678f9f9
Remmy <98552953+R-Owino@users.noreply.github.com>
2023-10-04 00:50:45 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-/* Function that returns a string of all the set values that start with a specific string (startString)
+/* Function that returns a string of all the set values that start with a specific string
     * @param {string} startString - the string that the set values should start with
     * @param {Set} set - the set that the values should be taken from
  * When value starts with StartString, append the rest of the value to the set
@@ -6,7 +6,17 @@
  */
 
 export default function cleanSet(set, startString) {
-    const matchingValues = [...set].filter(element => element.startsWith(startString));
-    const output = matchingValues.map(value => value.slice(startString.length)).join('-');
+  let output = '';
+
+  if (!startString || !startString.length) {
     return output;
+  }
+
+  for (const elem of set) { // eslint-disable-line no-restricted-syntax
+    if (elem && elem.startsWith(startString)) {
+      output += `${elem.slice(startString.length)}-`;
+    }
+  }
+
+  return output.slice(0, output.length - 1);
 }
