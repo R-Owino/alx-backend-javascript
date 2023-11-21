5b44ccc7ce8465df5e7307e8dac416d511bce8aa
R-Owino <sayeakinyi@gmail.com>
2023-11-21 21:59:00 +0300

    Chore: Use process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,25 +4,18 @@
  * When the program ends, it should display: This important software is now closing
  */
 
-const readline = require('readline');
-
-// create readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 // display welcome message
 console.log('Welcome to Holberton School, what is your name?');
 
-// prompt user for input
-rl.question('', (name) => {
-  // display user input
-  console.log(`Your name is: ${name}`);
+// read user input
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
-  // close readline interface
-  rl.close();
-
-  // display closing message
-  console.log('This important software is now closing');
+// display closing message
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
