5f0efabf74b57d8699775d871e1c02dd53a4c3d8
R-Owino <sayeakinyi@gmail.com>
2023-11-22 00:02:34 +0300

    Chore: Create a more complex HTTP server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,28 +7,27 @@
 
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-const path = require('path');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    if (req.url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        countStudents(process.argv[2])
-        .then((data) => {
-            res.end(`This is the list of our students\n${data}`);
-        })
-        .catch((error) => {
-            res.end(error.message);
-        });
-    } else {
-        res.end('Hello Holberton School!');
-    }
-    });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
 
 app.listen(port);
 
