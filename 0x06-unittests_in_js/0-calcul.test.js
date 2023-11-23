a8be84423d3931513645c2ffb2d9fdb814706b96
R-Owino <sayeakinyi@gmail.com>
2023-11-23 14:12:54 +0300

    Update: Add more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,22 +3,45 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-    it('should round numbers and return the sum', () => {
-        // case 1
+describe('Testing calculateNumber', () => {
+    it('Add 2 numbers', () => {
         const res1 = calculateNumber(1, 3);
         assert.strictEqual(res1, 4);
+    });
 
-        // case 2
+    it('Check rounding', () => {
         const res2 = calculateNumber(1, 3.7);
         assert.strictEqual(res2, 5);
+    });
 
-        // case 3
+    it('Check rounding when both args are double', () => {
         const res3 = calculateNumber(1.2, 3.7);
         assert.strictEqual(res3, 5);
-
-        // case 4
-        const res4 = calculateNumber(1.5, 3.7);
-        assert.strictEqual(res4, 6);
     });
+
+    it('Check rounding when first arg is double', () => {
+        const res3 = calculateNumber(1.2, 3);
+        assert.strictEqual(res3, 4);
+    });
+
+    it('Check rounding when second arg is double', () => {
+        const res3 = calculateNumber(1, 3.7);
+        assert.strictEqual(res3, 5);
+    });
+
+    it('Check rounding when both args are negative', () => {
+        const res3 = calculateNumber(-1.2, -3.7);
+        assert.strictEqual(res3, -5);
+    });
+
+    it('Check rounding when first arg is negative', () => {
+        const res3 = calculateNumber(-1.2, 3);
+        assert.strictEqual(res3, 2);
+    });
+
+    it('Check rounding when second arg is negative', () => {
+        const res3 = calculateNumber(1, -3.7);
+        assert.strictEqual(res3, -3);
+    });
+
 });
