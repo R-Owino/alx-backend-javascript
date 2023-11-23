13eab4e8b069927b632b24b267eda034c6feb000
R-Owino <sayeakinyi@gmail.com>
2023-11-23 14:35:39 +0300

    Update: calculateNumber function

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+
 /* function calculateNumber that accepts 2 arguments (a, b) and returns their sum. */
 
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+const calculateNumber = (a, b) => {
+    return Math.round(a) + Math.round(b);
+};
 
 module.exports = calculateNumber;
