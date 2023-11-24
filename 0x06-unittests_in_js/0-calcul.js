d754e4c4f8c64714f05d3128c4424d6ab00b709e
R-Owino <sayeakinyi@gmail.com>
2023-11-24 08:42:34 +0300

    Update: Function and its tests

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -3,7 +3,7 @@
 /* function calculateNumber that accepts 2 arguments (a, b) and returns their sum. */
 
 const calculateNumber = (a, b) => {
-    return Math.round(a) + Math.round(b);
+    return (Math.round(a) + Math.round(b));
 };
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,43 +5,35 @@ const calculateNumber = require('./0-calcul');
 
 describe('Testing calculateNumber', () => {
     it('Add 2 numbers', () => {
-        const res1 = calculateNumber(1, 3);
-        assert.strictEqual(res1, 4);
+        assert.equal(calculateNumber(1, 3), 4);
     });
 
     it('Check rounding', () => {
-        const res2 = calculateNumber(1, 3.7);
-        assert.strictEqual(res2, 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
     });
 
     it('Check rounding when both args are double', () => {
-        const res3 = calculateNumber(1.2, 3.7);
-        assert.strictEqual(res3, 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
     });
 
     it('Check rounding when first arg is double', () => {
-        const res3 = calculateNumber(1.2, 3);
-        assert.strictEqual(res3, 4);
+        assert.equal(calculateNumber(1.2, 3), 4);
     });
 
     it('Check rounding when second arg is double', () => {
-        const res3 = calculateNumber(1, 3.7);
-        assert.strictEqual(res3, 5);
-    });
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });    
 
     it('Check rounding when both args are negative', () => {
-        const res3 = calculateNumber(-1.2, -3.7);
-        assert.strictEqual(res3, -5);
+        assert.equal(calculateNumber(-1.2, -3.7), -5);
     });
 
     it('Check rounding when first arg is negative', () => {
-        const res3 = calculateNumber(-1.2, 3);
-        assert.strictEqual(res3, 2);
+        assert.equal(calculateNumber(-1.2, 3), 2);
     });
 
     it('Check rounding when second arg is negative', () => {
-        const res3 = calculateNumber(1, -3.7);
-        assert.strictEqual(res3, -3);
+        assert.equal(calculateNumber(1, -3.7), -3);
     });
 
 });
